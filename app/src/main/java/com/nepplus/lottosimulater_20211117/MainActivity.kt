0c49e224b442ebc45e127513e9797472a8219d1e
이아현 <suk03029@gamil.com>
2021-11-17 10:07:10 +0900

    코틀린 반목문 체험

--- app/src/main/java/com/nepplus/lottosimulater_20211117/MainActivity.kt
+++ app/src/main/java/com/nepplus/lottosimulater_20211117/MainActivity.kt
@@ -2,6 +2,8 @@ package com.nepplus.lottosimulater_20211117
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,11 +12,28 @@ class MainActivity : AppCompatActivity() {
 
         setupEvent()
         setValues()
+        makeLottoNumbers()
 
 
     }
 
     fun setupEvent(){
+
+    btnBuyLotto.setOnClickListener {
+//        로또 번호 6개 생성
+//        보너스 번호 생성
+
+    }
+
+
+
+    }
+
+    fun makeLottoNumbers(){
+//        6개의 로또 번호 생성
+        for (i in 0 until 6) {
+            Log.d("숫자확인",i.toString())
+        }
 
     }
 
