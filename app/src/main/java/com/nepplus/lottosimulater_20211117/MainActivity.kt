ff8bd0cefb39ca497eec5e09f9329318cb85708b
이아현 <suk03029@gamil.com>
2021-11-17 12:38:19 +0900

    자동 반복 구매 시작   - 반목 할인 코드 별도 변수

--- app/src/main/java/com/nepplus/lottosimulater_20211117/MainActivity.kt
+++ app/src/main/java/com/nepplus/lottosimulater_20211117/MainActivity.kt
@@ -2,8 +2,11 @@ package com.nepplus.lottosimulater_20211117
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.NumberFormat
 import java.util.*
@@ -14,19 +17,44 @@ class MainActivity : AppCompatActivity() {
     val mWinLottoNumArr = ArrayList<Int>()
     var mBonusNum = 0
 
-//    내가 쓴 금액?
+    //    내가 쓴 금액?
     var mUsedMoney = 0L
-//    당첨된 금액
+
+    //    당첨된 금액
     var mEarnedMoney = 0L
 
 
     lateinit var mLottoNumTxtList: ArrayList<TextView>
 
-    val mMyLottoNumArr = arrayListOf(35,14,17,38,44,7)
+    val mMyLottoNumArr = arrayListOf(35, 14, 17, 38, 44, 7)
 
 //    ArrayList해야 함
 
-    val mRankCountList = arrayListOf(0,0,0,0,0,0)
+    val mRankCountList = arrayListOf(0, 0, 0, 0, 0, 0)
+
+    //    할일을 관리하는 클래스
+    lateinit var mHandler: Handler
+
+    //    할일이 뭔지 작성
+    val buyLottoRunnable = object : Runnable {
+        override fun run() {
+            if (mUsedMoney <=10000000){
+                makeLottoNumbers()
+                makeBonudNum()
+                checkLottoRank()
+
+                mHandler.post(this)
+            }
+            else{
+                Toast.makeText(this@MainActivity, "자동 구매를 종료합니다.", Toast.LENGTH_SHORT).show()
+//        할 일을 추가 등록하지 않음
+
+            }
+        }
+
+    }
+//      쓴 돈이 1천만원 안된다면 다시 로또를 구매
+//        아니라면 더이상 반복X
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,6 +66,21 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupEvent() {
+
+        btnAutoBuyLotto.setOnClickListener {
+
+
+            while (true) {
+                makeLottoNumbers()
+                makeBonudNum()
+                checkLottoRank()
+                if (mUsedMoney >= 1000000) {
+                    break
+                }
+            }
+
+            mHandler.post(buyLottoRunnable)
+        }
 
 
         btnBuyLotto.setOnClickListener {
@@ -51,63 +94,66 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun checkLottoRank(){
+    fun checkLottoRank() {
 
 //        1천원 사용한 것으로 간주
-        mUsedMoney +=1000
+        mUsedMoney += 1000
 
 //      내 숫자 6개가 당첨된 번호 6개 중 몇개나 맞췄는지?
 
         var correctCount = 0
 
-        for ( myNum in mMyLottoNumArr) {
+        for (myNum in mMyLottoNumArr) {
 //            맞췄는가? -> 내 번호가 당첨번호에 있는가?
-            if(mWinLottoNumArr.contains(myNum)){
+            if (mWinLottoNumArr.contains(myNum)) {
                 correctCount++
             }
         }
 
-        Log.d("맞춘 갯수","${correctCount}")
+        Log.d("맞춘 갯수", "${correctCount}")
 
 //        등수판단 : when활용
 
-        when(correctCount){
-            6-> {
-                Log.d("등수","1등입니다." )
+        when (correctCount) {
+            6 -> {
+                Log.d("등수", "1등입니다.")
                 mEarnedMoney += 5000000000
                 mRankCountList[0]++
-                txtRank1.text="${mRankCountList[0]}회"
+                txtRank1.text = "${mRankCountList[0]}회"
             }
-            5-> {
+            5 -> {
 //                보너스 번호 검사 해야 함 => 보너스 번호가 내 번호 안에 있는가?
-                if(mMyLottoNumArr.contains(mBonusNum)){
+                if (mMyLottoNumArr.contains(mBonusNum)) {
                     Log.d("등수", "2등입니다.")
                     mEarnedMoney += 50000000
                     mRankCountList[1]++
-                    txtRank2.text="${mRankCountList[1]}회"
-                }
-                else{Log.d("등수", "3등입니다.")
+                    txtRank2.text = "${mRankCountList[1]}회"
+                } else {
+                    Log.d("등수", "3등입니다.")
                     mEarnedMoney += 2000000
                     mRankCountList[2]++
-                    txtRank3.text="${mRankCountList[2]}회"
+                    txtRank3.text = "${mRankCountList[2]}회"
                 }
 
-                }
-            4-> {Log.d("등수", "4등입니다.")
-                mEarnedMoney +=50000
+            }
+            4 -> {
+                Log.d("등수", "4등입니다.")
+                mEarnedMoney += 50000
                 mRankCountList[3]++
-                txtRank4.text="${mRankCountList[3]}회"
+                txtRank4.text = "${mRankCountList[3]}회"
 
             }
-            3->{Log.d("등수", "5등입니다.")
-                mUsedMoney -=5000
+            3 -> {
+                Log.d("등수", "5등입니다.")
+                mUsedMoney -= 5000
                 mRankCountList[4]++
-                txtRank5.text="${mRankCountList[4]}회"
+                txtRank5.text = "${mRankCountList[4]}회"
 
             }
-            else-> {Log.d("등수", "꽝 입니다.")
+            else -> {
+                Log.d("등수", "꽝 입니다.")
                 mRankCountList[5]++
-                txtRank6.text="${mRankCountList[5]}회"
+                txtRank6.text = "${mRankCountList[5]}회"
 
             }
         }
@@ -116,16 +162,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun makeBonudNum() {
-        while (true){
+        while (true) {
             val randomNum = (1..45).random()
             val isDupOk = !mWinLottoNumArr.contains(randomNum)
-            if (isDupOk)  {
-                mBonusNum= randomNum
+            if (isDupOk) {
+                mBonusNum = randomNum
                 break
             }
         }
 //        보너스 번호 텍스트뷰에 반영
-        txtLottoBonusNum.text= mBonusNum.toString()
+        txtLottoBonusNum.text = mBonusNum.toString()
     }
 
 
@@ -182,6 +228,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setValues() {
+
+        mHandler = Handler(Looper.getMainLooper())
+
+
 ///////////이 방법은 귀찮아서 사용하지 않음
 //        mLottoNumTxtList = ArrayList<TextView>()
 //        mLottoNumTxtList.add.
