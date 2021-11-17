789dd7154a89f58ac158e4abe4b7a03f48bb4364
이아현 <suk03029@gamil.com>
2021-11-17 10:42:44 +0900

    text view 6개와 당첨번호 6개를 연결 - arrayListof: 객체 생성과 목록 반영도 해줌

--- app/src/main/java/com/nepplus/lottosimulater_20211117/MainActivity.kt
+++ app/src/main/java/com/nepplus/lottosimulater_20211117/MainActivity.kt
@@ -3,13 +3,13 @@ package com.nepplus.lottosimulater_20211117
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mWinLottoNumArr = ArrayList<Int>()
-
-
+    lateinit var mLottoNumTxtList : ArrayList<TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,47 +17,42 @@ class MainActivity : AppCompatActivity() {
 
         setupEvent()
         setValues()
-        makeLottoNumbers()
+   }
+
+    fun setupEvent() {
 
 
-    }
-
-    fun setupEvent(){
-
-
-
-
-    btnBuyLotto.setOnClickListener {
+        btnBuyLotto.setOnClickListener {
 //        로또 번호 6개 생성
+        makeLottoNumbers()
 //        보너스 번호 생성
 
+        }
+
+
     }
 
 
-
-    }
-
-
-    fun makeLottoNumbers(){
+    fun makeLottoNumbers() {
 
 /////////////////////////////////////////
         mWinLottoNumArr.clear()
 
 //        6개의 로또 번호 생성
         for (i in 0 until 6) {
-            Log.d("숫자확인",i.toString())
+            Log.d("숫자확인", i.toString())
 
 //        랜덤 숫자 추출 -> 제대로 된 숫자라면 목록에 추가
-            while(true){
-                val randonNum = (Math.random() *45 +1).toInt()
-                Log.d("랜덤",randonNum.toString())
+            while (true) {
+                val randonNum = (Math.random() * 45 + 1).toInt()
+                Log.d("랜덤", randonNum.toString())
 
 //                ////////////////////중복검사///////////////////////
 
                 val isDupOk = !mWinLottoNumArr.contains(randonNum)
 
 
-                if(isDupOk){
+                if (isDupOk) {
 //                    숫자를 당첨 번호로 추가
 
                     mWinLottoNumArr.add(randonNum)
@@ -72,13 +67,29 @@ class MainActivity : AppCompatActivity() {
 
 //        당첨 번호 6개 확인
 
-        for( num in mWinLottoNumArr){
-            Log.d("당첨번호", num.toString())
+        for (i in 0 until 6 ){
+//            txtView를 i 번 째 것을 커내 비교해보자
+
+            mLottoNumTxtList[i].text = mWinLottoNumArr[i].toString()
         }
+
+
+//        for (i in 0 until 6){
+//            mLottoNumTxtList[i].text = mWinLottoNumArr[i].toString()
+//
+//        }
+
+//        for (num in mWinLottoNumArr) {
+//            Log.d("당첨번호", num.toString())
+//        }
 
     }
 
-    fun setValues(){
+    fun setValues() {
+///////////이 방법은 귀찮아서 사용하지 않음
+//        mLottoNumTxtList = ArrayList<TextView>()
+//        mLottoNumTxtList.add.
+        mLottoNumTxtList = arrayListOf(txtLottoNum1,txtLottoNum2,txtLottoNum3,txtLottoNum4,txtLottoNum5,txtLottoNum6)
 
     }
 }
