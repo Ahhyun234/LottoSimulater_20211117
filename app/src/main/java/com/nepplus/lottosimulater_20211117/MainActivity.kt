31aef4f95e6595d17aa2c41ffd71cb87a836d202
이아현 <suk03029@gamil.com>
2021-11-17 10:13:13 +0900

    랜덤 숫자 6ㄱ개 생성 중복 검사는 안함

--- app/src/main/java/com/nepplus/lottosimulater_20211117/MainActivity.kt
+++ app/src/main/java/com/nepplus/lottosimulater_20211117/MainActivity.kt
@@ -6,6 +6,11 @@ import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val mWinLottoNumArr = ArrayList<Int>()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +24,9 @@ class MainActivity : AppCompatActivity() {
 
     fun setupEvent(){
 
+
+
+
     btnBuyLotto.setOnClickListener {
 //        로또 번호 6개 생성
 //        보너스 번호 생성
@@ -29,10 +37,26 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     fun makeLottoNumbers(){
+
+/////////////////////////////////////////
+        mWinLottoNumArr.clear()
+
 //        6개의 로또 번호 생성
         for (i in 0 until 6) {
             Log.d("숫자확인",i.toString())
+
+//        랜덤 숫자 추출 -> 제대로 된 숫자라면 목록에 추가
+            while(true){
+                val randonNum = (Math.random() *45 +1).toInt()
+                Log.d("랜덤",randonNum.toString())
+
+                if(true){
+                    break;
+                }
+            }
+
         }
 
     }
